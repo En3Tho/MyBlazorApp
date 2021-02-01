90c1cca020d3c0e210ec6e4023c06d87c99a5bf7
En3Tho <37334640+En3Tho@users.noreply.github.com>
2021-02-01 16:58:31 +0300

    Added subscribing component base

--- src/MyBlazorApp.BlazorClient.Backend/Models.fs
+++ src/MyBlazorApp.BlazorClient.Backend/Models.fs
@@ -12,26 +12,29 @@ type private ComponentValueDictionary<'TType, 'TKey, 'TValue when 'TKey: equalit
 
     [<DefaultValue>]
     static val mutable private _KeyValueBag: Dictionary<'TKey, 'TValue>
-    static do ComponentValueDictionary<'TType, 'TKey, 'TValue>._KeyValueBag <- Dictionary()
-    static member KeyValueBag = ComponentValueDictionary<'TType, 'TKey, 'TValue>._KeyValueBag
 
+    static do ComponentValueDictionary<'TType, 'TKey, 'TValue>._KeyValueBag <- Dictionary()
+
+    static member KeyValueBag =
+        ComponentValueDictionary<'TType, 'TKey, 'TValue>
+            ._KeyValueBag
 
 type ComponentDataProvider(_logger: ILogger<ComponentDataProvider>) =
     member this.GetOrCreateNew<'TType, 'TKey, 'TValue when 'TKey: equality and 'TValue: (new: unit -> 'TValue)> key =
         let bag = ComponentValueDictionary<'TType, 'TKey, 'TValue>.KeyValueBag
+        
         match bag |> Dictionary.tryGetValue key with
-        | Some value ->
-            value
+        | Some value -> value
         | None ->
-            let value = Object.createNew<'TValue>()
+            let value = Object.createNew<'TValue> ()
             bag.[key] <- value
             value
 
     member this.GetOrCreateWith<'TType, 'TKey, 'TValue when 'TKey: equality> key (factory: 'TValue Func) =
         let bag = ComponentValueDictionary<'TType, 'TKey, 'TValue>.KeyValueBag
+        
         match bag |> Dictionary.tryGetValue key with
-        | Some value ->
-            value
+        | Some value -> value
         | None ->
             let value = factory.Invoke()
             bag.[key] <- value
@@ -46,23 +49,56 @@ type ComponentDataProvider(_logger: ILogger<ComponentDataProvider>) =
         bag.Remove key |> ignore
 
 [<Struct>]
-type ComponentEventHandler =
-    [<DefaultValue>]
-    val mutable private handler: EventHandler
-    member this.Subscribe<'b when 'b :> ComponentData>(action: Action, [<ParamArray>] datas: 'b[]) =
-        this.handler <- EventHandler (fun _ _ -> action.Invoke())
-        for data in datas do data.OnChange.AddHandler this.handler
+type ComponentDataChangeEventHandler =
+    val mutable private _handler: EventHandler
+    val mutable private _componentDatas: ComponentData[]
 
-    member this.Unsubscribe<'b when 'b :> ComponentData>([<ParamArray>] datas: 'b[]) =
-        for data in datas do data.OnChange.RemoveHandler this.handler
-        this.handler <- null
+    member this.Subscribe(action: Action, [<ParamArray>] datas: ComponentData[]) =
+        this._handler <- EventHandler(fun _ _ -> action.Invoke())
+        this._componentDatas <- datas
+        for data in datas do data.OnChange.AddHandler this._handler
+
+    member this.Subscribe(action: Action, data1) =
+        this._handler <- EventHandler(fun _ _ -> action.Invoke())
+        this._componentDatas <- [| data1 |]
+        data1.OnChange.AddHandler this._handler
+
+    member this.Subscribe(action: Action, data1, data2) =
+        this._handler <- EventHandler(fun _ _ -> action.Invoke())
+        this._componentDatas <- [| data1; data2 |]
+        data1.OnChange.AddHandler this._handler
+        data2.OnChange.AddHandler this._handler
+
+    member this.Subscribe(action: Action, data1, data2, data3) =
+        this._handler <- EventHandler(fun _ _ -> action.Invoke())
+        this._componentDatas <- [| data1; data2; data3 |]
+        data1.OnChange.AddHandler this._handler
+        data2.OnChange.AddHandler this._handler
+        data3.OnChange.AddHandler this._handler
+    
+    member this.Unsubscribe() =
+        if this._componentDatas <> null then
+            for data in this._componentDatas do
+                data.OnChange.RemoveHandler this._handler
+        this._handler <- null
+        this._componentDatas <- null
 
 type CounterData() =
     inherit ComponentData()
     let mutable current = 0
     let mutable totalClicks = 0
-    member this.Current with get() = current and set (value) = current <- value; this.OnDataChanged()
-    member this.TotalClicks with get() = totalClicks and set (value) = totalClicks <- value; this.OnDataChanged()
+
+    member this.Current
+        with get () = current
+        and set (value) =
+            current <- value
+            this.OnDataChanged()
+
+    member this.TotalClicks
+        with get () = totalClicks
+        and set (value) =
+            totalClicks <- value
+            this.OnDataChanged()
 
 type Theme =
     | Black = 0
@@ -72,7 +108,13 @@ type Theme =
 type ThemeSwitch(theme: Theme) =
     inherit ComponentData()
     let mutable theme = theme
-    member this.Theme with get() = theme and set (value) = theme <- value; this.OnDataChanged();
+
+    member this.Theme
+        with get () = theme
+        and set (value) =
+            theme <- value
+            this.OnDataChanged()
+
     member this.ThemeString =
         match this.Theme with
         | Theme.Black -> "black-Theme"
