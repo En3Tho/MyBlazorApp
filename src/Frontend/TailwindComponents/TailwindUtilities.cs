1f40b015a2ebd1d18353d13cbc1d8280e7ca58c7
En3Tho- <razerael@gmail.com>
2022-11-04 16:10:15 +0300

    slightly improved emailform

--- src/Frontend/TailwindComponents/TailwindUtilities.cs
+++ src/Frontend/TailwindComponents/TailwindUtilities.cs
@@ -5,3 +5,27 @@ public static class HtmlUtilities
     public static string If(bool condition, string style) => condition ? style : "";
     public static string If(bool condition, string style, string other) => condition ? style : other;
 }
+
+// TODO: remove this if it doesn't really help (so far it didn't)
+public class Ref<T>
+{
+    public Ref(T value)
+    {
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return Value!.ToString()!;
+    }
+
+    public override int GetHashCode()
+    {
+        return Value!.GetHashCode();
+    }
+
+    public T Value { get; set; }
+
+    public static implicit operator T (Ref<T> @ref) => @ref.Value;
+    public static implicit operator Ref<T> (T value) => new(value);
+}
