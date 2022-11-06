bb952444d9a2c8dec8ec568c878a083d8a68be76
En3Tho- <razerael@gmail.com>
2022-11-06 15:18:16 +0300

    Fun with render fragments

--- src/Frontend/TailwindComponents/TailwindUtilities.cs
+++ src/Frontend/TailwindComponents/TailwindUtilities.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Components;
+
 namespace TailwindComponents;
 
 public static class HtmlUtilities
 {
     public static string If(bool condition, string style) => condition ? style : "";
     public static string If(bool condition, string style, string other) => condition ? style : other;
+    public static RenderFragment If(bool condition, RenderFragment style) => condition ? style : _ => { };
+    public static RenderFragment If(bool condition, RenderFragment style, RenderFragment other) => condition ? style : other;
+    public static RenderFragment<T> If<T>(bool condition, RenderFragment<T> style) => condition ? style : _ => _ => { };
+    public static RenderFragment<T> If<T>(bool condition, RenderFragment<T> style, RenderFragment<T> other) => condition ? style : other;
 }
 
 // TODO: remove this if it doesn't really help (so far it didn't)
