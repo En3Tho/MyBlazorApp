93248b7e46554660b95610ed5feb424acfe9f815
En3Tho- <razerael@gmail.com>
2022-11-09 18:35:08 +0300

    net7

--- src/Frontend/TailwindComponents/CodinGame/Command.cs
+++ src/Frontend/TailwindComponents/CodinGame/Command.cs
@@ -4,80 +4,98 @@ namespace TailwindComponents.CodinGame;
 
 internal record struct Command(int idx1, int idx2, char op)
 {
-    private static readonly char[] _digits =
-    {
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    };
-
-    private static readonly char[] _ops =
-    {
-        '+', '-', '*', '/'
-    };
-
     public static bool TryParse(string value, out Command command)
     {
-        if (value is ['$', var idx1, ' ', var op, ' ', '$', var idx2]
-            && idx1 != idx2
-            && _digits.Contains(idx1)
-            && _digits.Contains(idx2) && _ops.Contains(op))
+        var (success, parsedCommand) = value switch
         {
-            command = new(idx1 - '0', idx2 - '0', op);
-            return true;
-        }
+            ['$', var idx1, ' ', ('+' or '-' or '@') and var op, ' ', '$', var idx2] when
+                idx1 != idx2
+                && char.IsDigit(idx1)
+                && char.IsDigit(idx2) => (true, new Command(idx1 - '0', idx2 - '0', op)),
+            ['$', var idx1, ' ', ('*' or '/') and var op, ' ', var idx2] when
+                idx2 > 0
+                && char.IsDigit(idx1)
+                && char.IsDigit(idx2) => (true, new (idx1 - '0', idx2 - '0', op)),
+            _ => (false, new())
+        };
 
-        command = default;
-        return false;
+        command = parsedCommand;
+        return success;
     }
 
     public override string ToString()
     {
-        return $"${idx1} {op} ${idx2}";
+        var ch = op is '*' or '/' ? "" : "$";
+        return $"${idx1} {op} {ch}{idx2}";
     }
 
-    Func<int, int, (int, int)> GetProcessor(bool reverse)
+    Func<int, int, (int, int)> GetRowProcessor()
     {
-        if (reverse)
-        {
-            return op switch
-            {
-                '+' => (a, b) => (a - b, b),
-                '-' => (a, b) => (a + b, b),
-                '*' => (a, b) => (a / b, b),
-                '/' => (a, b) => (a * b, b),
-                '@' => (a, b) => (a, b),
-                _ => throw new ArgumentOutOfRangeException(nameof(op))
-            };
-        }
-
         return op switch
         {
             '+' => (a, b) => (a + b, b),
             '-' => (a, b) => (a - b, b),
-            '*' => (a, b) => (a * b, b),
-            '/' => (a, b) => (a / b, b),
             '@' => (a, b) => (b, a),
             _ => throw new ArgumentOutOfRangeException(nameof(op))
         };
     }
 
-    public bool Run(int[,] values, bool reverse, [NotNullWhen(false)] out string? error)
+    Func<int, int, int> GetConstProcessor()
     {
-        if ((uint)idx1 >= values.GetLength(0) || (uint)idx2 >= values.GetLength(0))
+        return op switch
+        {
+            '*' => (a, b) => a * b,
+            '/' => (a, b) => a / b,
+            _ => throw new ArgumentOutOfRangeException(nameof(op))
+        };
+    }
+
+    private bool RunConst(int[,] values, [NotNullWhen(false)] out string? error)
+    {
+        if ((uint) idx1 >= values.GetLength(0))
         {
             error = "Index out of range";
             return false;
         }
 
-        var processor = GetProcessor(reverse);
+        var constProcessor = GetConstProcessor();
+        for (var colIdx = 0; colIdx < values.GetLength(1); colIdx++)
+        {
+            values[idx1, colIdx] = constProcessor(values[idx1, colIdx], idx2);
+        }
+
+        error = null;
+        return true;
+    }
+
+    private bool RunRow(int[,] values, [NotNullWhen(false)] out string? error)
+    {
+        if ((uint) idx1 >= values.GetLength(0) || (uint) idx2 >= values.GetLength(0))
+        {
+            error = "Index out of range";
+            return false;
+        }
+
+        var rowProcessor = GetRowProcessor();
 
         for (var colIdx = 0; colIdx < values.GetLength(1); colIdx++)
         {
-            var (m1, m2) = processor(values[idx1, colIdx], values[idx2, colIdx]);
+            var (m1, m2) = rowProcessor(values[idx1, colIdx], values[idx2, colIdx]);
             values[idx1, colIdx] = m1;
             values[idx2, colIdx] = m2;
         }
 
         error = null;
         return true;
+    }
+
+
+    public bool Run(int[,] values, [NotNullWhen(false)] out string? error)
+    {
+        if (op is '+' or '-' or '@')
+        {
+            return RunRow(values, out error);
+        }
+        return RunConst(values, out error);
     }
 }
