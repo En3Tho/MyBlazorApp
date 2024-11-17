a6a12a66ce2b34cde9ad7d64668f01d3ab7b6e4b
En3Tho- <razerael@gmail.com>
2024-11-17 12:38:05 +0300

    update to .Net 9

--- src/Hosting/MyBlazorApp.AppHost/Program.cs
+++ src/Hosting/MyBlazorApp.AppHost/Program.cs
@@ -16,18 +16,18 @@ var wasmhost = builder.AddProject<Projects.MyBlazorApp_Server_WebAssemblyHost>("
 var telemetryProxy = builder.AddProject<Projects.TelemetryProxy>("telemetry-proxy", "https");
 
 builder
-    .ForAll([backend, blazorserver, photino, wasmhost, telemetryProxy], resourceBuilder =>
-        resourceBuilder.WithLogLevel(LogLevel.Trace))
-    .ForAll([blazorserver, photino, wasmhost], resourceBuilder =>
+    .ForAll([ backend, blazorserver, photino, wasmhost, telemetryProxy ], resourceBuilder =>
+        resourceBuilder.WithLogLevel(LogLevel.Information))
+    .ForAll([ blazorserver, photino, wasmhost ], resourceBuilder =>
         resourceBuilder
             .WithReferences(backend));
 
-builder.UseElasticExporter([ backend, blazorserver, photino, wasmhost ]);
+// builder.UseElasticExporter([ backend, blazorserver, photino, wasmhost ]);
 // builder.UseTelemetryProxy([backend, blazorserver, photino, wasmhost]);
 
 wasmhost
     .WithEnvironmentVariable("WASM__OTEL_SERVICE_NAME", "wasm")
-    .WithLogLevel("WASM__", LogLevel.Error)
+    .WithLogLevel("WASM__", LogLevel.Information)
     .WithEnvironmentCopy("WASM__");
 
 builder.Build().Run();
